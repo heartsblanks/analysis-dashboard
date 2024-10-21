440ddffb7efb4b6314e9c96beed46bec8a1c70d6
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-10-21 10:07:14 +0200

    Update bubbles.component.ts

--- client/src/app/bubbles/bubbles.component.ts
+++ client/src/app/bubbles/bubbles.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-bubbles',
   templateUrl: './bubbles.component.html',
-  styleUrls: ['./bubbles.component.scss']
+  styleUrls: ['./bubbles.component.scss'],
+  animations: [
+    trigger('bubbleAnimation', [
+      transition(':leave', [
+        style({ transform: 'scale(1)', opacity: 1 }),
+        animate('500ms', style({ transform: 'scale(0)', opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class BubblesComponent {
-  paps = ['PAP1', 'PAP2', 'PAP3'];
+  paps: string[] = ['PAP1', 'PAP2', 'PAP3', 'PAP4'];  // Initial set of PAPs (could come from API)
+  filteredPaps: string[] = this.paps;  // The filtered list of PAPs
+  filterText = '';  // The text input by the user
+
+  filterPaps() {
+    // Filter based on user input
+    this.filteredPaps = this.paps.filter(pap => 
+      pap.toLowerCase().startsWith(this.filterText.toLowerCase()));
+  }
 }
