ae8ee37493a73a0c857ef1502b3a3fe3929d9f4b
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-10-21 10:08:20 +0200

    Update app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { BubblesComponent } from './bubbles/bubbles.component';
 
 @NgModule({
   declarations: [AppComponent, BubblesComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
