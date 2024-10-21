45c431395775260faa2ed70ddddc70976006cd3b
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-10-21 10:00:36 +0200

    Update app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
-import { PapBubbleComponent } from './components/pap-bubble/pap-bubble.component';
+import { BubblesComponent } from './bubbles/bubbles.component';
 
 @NgModule({
-  declarations: [AppComponent, PapBubbleComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, BubblesComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
