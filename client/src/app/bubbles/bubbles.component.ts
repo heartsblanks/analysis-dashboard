24303b4f9de7817b254711814079f58108f71b81
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-10-21 10:01:07 +0200

    Create bubbles.component.ts

---
+++ client/src/app/bubbles/bubbles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bubbles',
+  templateUrl: './bubbles.component.html',
+  styleUrls: ['./bubbles.component.scss']
+})
+export class BubblesComponent {
+  paps = ['PAP1', 'PAP2', 'PAP3'];
+}
