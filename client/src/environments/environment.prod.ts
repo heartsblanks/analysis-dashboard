58a4283d33eba45b63e162f4b139893ddf96a1e2
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-10-21 10:41:39 +0200

    Create environment.prod.ts

---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+// client/src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  apiUrl: 'https://your-production-url.com'  // Replace with your production API URL
+};
