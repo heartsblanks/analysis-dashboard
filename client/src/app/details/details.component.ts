88a47e759c552f973281485a4cddb5e207da49b3
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-10-22 11:56:52 +0200

    Update details.component.ts

--- client/src/app/details/details.component.ts
+++ client/src/app/details/details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-details',
@@ -8,11 +9,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailsComponent implements OnInit {
   pap: string;
+  queues: any[] = [];
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit() {
     // Get the PAP name from the route parameters
     this.pap = this.route.snapshot.paramMap.get('pap');
+    this.loadQueues();
+  }
+
+  loadQueues() {
+    this.http.get<any[]>(`/api/paps/${this.pap}/queues`).subscribe(data => {
+      this.queues = data;
+    }, error => {
+      console.error('Error loading queues:', error);
+    });
   }
 }
