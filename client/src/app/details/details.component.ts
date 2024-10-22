0154e0b7c1faacfbce249c2241d53a21961a4769
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-10-22 11:18:29 +0200

    Update details.component.ts

--- client/src/app/details/details.component.ts
+++ client/src/app/details/details.component.ts
@@ -1,5 +1,11 @@
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
 export class DetailsComponent implements OnInit {
   pap: string;
 
