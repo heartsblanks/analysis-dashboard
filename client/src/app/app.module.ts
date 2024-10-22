eff1e26c4e27344b737eb66b22797d92e09d4c34
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-10-22 11:46:04 +0200

    Update app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,29 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';  // <-- Import RouterModule and Routes
+import { RouterModule, Routes } from '@angular/router';  // Import RouterModule and Routes
 
 import { AppComponent } from './app.component';
 import { BubblesComponent } from './bubbles/bubbles.component';
-import { DetailsComponent } from './details/details.component';  // <-- Import the new details component
+import { DetailsComponent } from './details/details.component';  // Import the new DetailsComponent
 
 // Define routes
 const routes: Routes = [
-  { path: '', component: BubblesComponent },  // Default route to display bubbles
-  { path: 'details/:pap', component: DetailsComponent }  // Route to display details when PAP is clicked
+  { path: '', component: BubblesComponent },  // Route to display bubbles
+  { path: 'details/:pap', component: DetailsComponent }  // Route to display details
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     BubblesComponent,
-    DetailsComponent  // <-- Declare DetailsComponent
+    DetailsComponent  // Declare DetailsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(routes)  // <-- Add RouterModule and pass in the routes
+    RouterModule.forRoot(routes)  // Configure RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
