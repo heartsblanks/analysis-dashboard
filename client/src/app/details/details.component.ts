141ce99676db5e854f8bd4b0eb0ebc8561331802
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-10-22 12:39:05 +0200

    Update details.component.ts

--- client/src/app/details/details.component.ts
+++ client/src/app/details/details.component.ts
@@ -10,6 +10,7 @@ import { HttpClient } from '@angular/common/http';
 export class DetailsComponent implements OnInit {
   pap: string;
   queues: any[] = [];
+  expandedQueues: boolean[] = [];  // <-- To track expanded/collapsed state
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
@@ -21,8 +22,13 @@ export class DetailsComponent implements OnInit {
   loadQueues() {
     this.http.get<any[]>(`/api/paps/${this.pap}/queues`).subscribe(data => {
       this.queues = data;
+      this.expandedQueues = new Array(this.queues.length).fill(false);  // Initialize collapsed state for each queue
     }, error => {
       console.error('Error loading queues:', error);
     });
+  }
+
+  toggleQueue(index: number) {
+    this.expandedQueues[index] = !this.expandedQueues[index];  // Toggle the state of the clicked queue
   }
 }
