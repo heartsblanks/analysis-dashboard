93476db74152f65e391c182e77af7d077babe190
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-12-02 09:12:17 +0100

    Update main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,14 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    HttpClientModule,
+    FormsModule,
+  ],
+}).catch(err => console.error(err));
