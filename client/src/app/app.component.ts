9b442a5949605ab66fc6fc020cde47d15353827d
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-12-02 10:31:40 +0100

    Update app.component.ts

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BubblesComponent } from './bubbles/bubbles.component';
+import { DetailsComponent } from './details/details.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [BubblesComponent],
+  imports: [
+    RouterModule.forRoot([
+      { path: '', component: BubblesComponent },
+      { path: 'details', component: DetailsComponent },
+    ]),
+  ],
 })
 export class AppComponent {}
